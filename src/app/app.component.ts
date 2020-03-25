7622bd8c3b4b500ce3220657b085c19a7755adc1
fatters <mattallen27@gmail.com>
2020-03-25 16:28:56 +0000

    fix service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+
+  constructor(private swUpdate: SwUpdate) {
+
+    this.swUpdate.available.subscribe(() => {
+      if (confirm('Update Available. Refresh the page now to update the cache.')) {
+        document.location.reload();
+      } else {
+        console.log('continue with the older version');
+      }
+    });
+
+    setInterval(() =>  {
+      this.swUpdate.checkForUpdate();
+    }, 21600);
+  }
+
+  private updateApp(): void {
+    document.location.reload();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { Route, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderModule } from './common/header/header.module';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
-import { PwaService } from './common/pwa/pwa.service';
 
 const routes: Route[] = [
   {path: '', loadChildren: './lists/lists.module#ListsModule'},
@@ -23,9 +22,6 @@ const routes: Route[] = [
     RouterModule.forRoot(routes),
     SweetAlert2Module.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
-  ],
-  providers: [
-    PwaService
   ],
   bootstrap: [AppComponent]
 })
